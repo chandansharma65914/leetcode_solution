7c2a37a7cb3caac9a7680153a066a0015ea9cbce
Chandan Sharma <109021997+chandansharma65914@users.noreply.github.com>
2023-12-13 23:22:12 +0530

    Time: 1 ms (73.11%), Space: 40.5 MB (26.89%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -4,17 +4,30 @@ class Solution {
         Map<Integer,Integer> map= new HashMap<>();
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
+                if(map.get(nums[i])==1){
+                     map.put(nums[i],map.get(nums[i])-1);
+                     sum-=nums[i];
+                }
+               
+               
             }else{
                 map.put(nums[i],1);
+                sum+=nums[i];
             }
         }
-        Set<Integer> set= map.keySet();
-        for(Integer i: set){
-            if(map.get(i)==1){
-                sum+=i;
-            }
-        }
+        
+        
+        
+        
+        
+        
+        
+        // Set<Integer> set= map.keySet();
+        // for(Integer i: set){
+        //     if(map.get(i)==1){
+        //         sum+=i;
+        //     }
+        // }
         return sum;
     }
 }
