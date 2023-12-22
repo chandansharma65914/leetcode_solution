d594821cb877c34d30042294e43864fd5bb3070d
Chandan Sharma <109021997+chandansharma65914@users.noreply.github.com>
2023-12-22 23:17:32 +0530

    Time: 54 ms (90.89%), Space: 45.3 MB (87.03%) - LeetHub

--- 0136-single-number/0136-single-number.ts
+++ 0136-single-number/0136-single-number.ts
@@ -1,12 +1,4 @@
-// function singleNumber(nums: number[]): number {
-    
-// };
 
-
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
 const singleNumber = (nums: number[]): number => {
     let ans:number = 0;
     for (let i :number = 0; i < nums.length; i++) {
