c16f54168622d907e1cdb17d79633f06baaf8cd8
Chandan Sharma <109021997+chandansharma65914@users.noreply.github.com>
2023-12-27 19:36:54 +0530

    Time: 1 ms (100.00%), Space: 44.7 MB (5.26%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.java
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.java
@@ -1,16 +1,11 @@
 class Solution {
     public int vowelStrings(String[] words, int left, int right) {
-        Map<Character,Integer> map= new HashMap<>();
-        map.put('a',1);
-         map.put('e',1);
-          map.put('i',1);
-           map.put('o',1);
-            map.put('u',1);
+     
         int count=0;
-        
+        String str="aeiou";
         for(int i=left;i<=right;i++){
-           String str= words[i];
-           if(map.containsKey(str.charAt(0)) && map.containsKey(str.charAt(str.length()-1))){
+          
+           if(str.contains(Character.toString(words[i].charAt(0))) && str.contains(Character.toString(words[i].charAt(words[i].length()-1)))){
                count++;
            }
         }
