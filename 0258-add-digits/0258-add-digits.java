d63e45a68c699b5d30eb23c8a034732310520413
Chandan Sharma <109021997+chandansharma65914@users.noreply.github.com>
2024-01-05 23:27:46 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (21.20%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,17 +1,7 @@
 class Solution {
-    public int sum(int n){
-        int ans=0;
-        while(n>0){
-            ans+=n%10;
-            n/=10;
-        }
-        return ans;
-    }
     public int addDigits(int num) {
-          
-        if(num<10){
-            return num;
-        }
-        return addDigits(sum(num));
+        if(num == 0) return 0;
+        else if(num % 9 == 0) return 9;
+        else return num % 9;
     }
 }
