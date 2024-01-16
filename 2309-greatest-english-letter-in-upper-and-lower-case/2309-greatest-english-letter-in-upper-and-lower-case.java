ed28d88c1a94d89ec55c315a0be8bc83d5aa4de7
Chandan Sharma <109021997+chandansharma65914@users.noreply.github.com>
2024-01-16 16:58:08 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (36.27%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.java
@@ -1,25 +1,12 @@
 class Solution {
     public String greatestLetter(String s) {
-        String ls= s.toLowerCase();
-        
-        boolean a[]= new boolean[26];
-
-        boolean b[]= new boolean[26];
-
-        for(int i=0;i<s.length();i++){
-           if(ls.charAt(i)==s.charAt(i)){
-               b[ls.charAt(i)-'a']=true;
-           } else{
-               a[s.charAt(i)-'A']=true;
-           }
-        }
-         for(int i=25;i>=0;i--){
-            if( a[i]==b[i] && a[i]){
-                return String.valueOf((char)(65+i));
+        for(int i='z';i>='a';i--){
+          String low= Character.toString(i);
+          String big= Character.toString(i-32);
+            if(s.contains(low)&& s.contains(big)){
+                return big;
             }
-         }
-
-
+        }
 return "";
     }
 }
