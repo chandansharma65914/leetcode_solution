ef50849b4d61ee634bd63a25356feeaa5f329a19
Chandan Sharma <109021997+chandansharma65914@users.noreply.github.com>
2025-11-02 03:54:12 +0530

    Time: 0 ms (100%), Space: 48 MB (9.07%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,23 +1,16 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-           int ans[]= {-1,-1};
-           ans[0]= search(nums,target,true);
-           ans[1]= search(nums,target,false);
-           return ans;
-    }
-    public int search(int[] nums , int target , boolean left){
+    public int searchLeftOrRight(int [] nums, int target, boolean left){
         int ans=-1;
-        int s=0;
+           int s=0;
         int e= nums.length-1;
-
         while(s<=e){
-            int m= s + (e-s)/2;
-            if(target< nums[m]){
-                e=m-1;
-            }else if(target> nums[m]){
+            int m = s + (e-s)/2;
+            if(nums[m]> target){
+                 e =m-1;
+            }else if (nums[m] < target){
                 s= m+1;
             }else{
-                ans= m;
+                ans =m;
                 if(left){
                     e= m-1;
                 }else{
@@ -26,5 +19,14 @@ class Solution {
             }
         }
         return ans;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        
+
+        int[] ans= {-1,-1};
+        ans[0]= searchLeftOrRight(nums,target,true);
+      
+         ans[1]= searchLeftOrRight(nums,target,false);
+     return ans;
     }
 }
